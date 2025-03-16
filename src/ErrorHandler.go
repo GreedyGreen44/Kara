16942905e26121633faaa95ffdaf32d5d588e853
GreedyGreen44 <yura-kov@inbox.ru>
2025-03-16 23:21:50 +1100

    Kara-related error descriptions

--- src/ErrorHandler.go
+++ src/ErrorHandler.go
@@ -5,27 +5,27 @@ func handleError(errorCode [2]byte, err error) int {
 	case 0x00: // non fatal
 		switch errorCode[1] {
 		case 0x01:
-			warningLog.Printf("Initializing warning, %v. Ailrun continue to work\n", err)
+			warningLog.Printf("Initializing warning, %v. Kara continue to work\n", err)
 		case 0x02:
-			warningLog.Printf("Reading response warning, %v. Ailrun continue to work\n", err)
+			warningLog.Printf("Reading response warning, %v. Kara continue to work\n", err)
 		default:
-			warningLog.Printf("Unexpected minor error code %v, Ailrun continue to work\n", int(errorCode[0]))
+			warningLog.Printf("Unexpected minor error code %v, Kara continue to work\n", int(errorCode[0]))
 		}
 		return 1
 	case 0x01: // fatal
 		switch errorCode[1] {
 		case 0x01:
-			errorLog.Printf("Initializing error, %v, Ailrun is shutting down\n", err)
+			errorLog.Printf("Initializing error, %v, Kara is shutting down\n", err)
 		case 0x02:
-			errorLog.Printf("Initializing error, %v, Ailrun is shutting down\n", err)
+			errorLog.Printf("Initializing error, %v, Kara is shutting down\n", err)
 		case 0x03:
-			errorLog.Printf("Creating request error, %v, Ailrun is shutting down\n", err)
+			errorLog.Printf("Creating request error, %v, Kara is shutting down\n", err)
 		default:
-			errorLog.Printf("Unexpected minor code %v, Ailrun is shutting down\n", int(errorCode[0]))
+			errorLog.Printf("Unexpected minor code %v, Kara is shutting down\n", int(errorCode[0]))
 		}
 		return 2
 	default:
-		errorLog.Printf("Unexpected senior error code %v, Ailrun is shutting down\n", int(errorCode[0]))
+		errorLog.Printf("Unexpected senior error code %v, Kara is shutting down\n", int(errorCode[0]))
 		return 2
 	}
 }
